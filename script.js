06bb31f681756cae1f0a78e2a2d920aaf0e7cf72
domagojrac <domagojradovcic1@gmail.com>
2022-09-26 17:02:00 +0200

    najtezi dio tek slijedi

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const buttons = document.querySelectorAll(".player");
+var generateButton = document.getElementById("generateButton");
+var count = 0;
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         button.classList.toggle("active");
+        buttons.forEach(button => {
+            if (button.classList.contains("active")) {
+                count++;  
+            };
+        });
+        generateButton.textContent = "Generate (" + count + ")";
+        count = 0;
     });
 }); 
+
+generateButton.addEventListener("click", () => {
+    buttons.forEach(button => {
+        if (button.classList.contains("active")) {
+            count++;  
+        };
+    });    
+    if (count < 12) {
+        alert("Nedostaje ti " + (12 - count) + " igrača!");
+    } else if (count > 12) {
+        alert("Imaš " + (count - 12) + " igrača previše!");
+    } else {
+        alert("Program još nije gotov! :(");
+    }
+    count = 0;
+});
