5dc21d0f296f2d925725af2fd0503e41a5c3f7f2
domagojrac <domagojradovcic1@gmail.com>
2022-09-26 19:51:55 +0200

    ajmooo

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const buttons = document.querySelectorAll(".player");
 var generateButton = document.getElementById("generateButton");
 var count = 0;
+const selectAudio = new Audio("Music/Game Menu Select Sound Effect (1).mp3");
+const unselectAudio = new Audio();
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         button.classList.toggle("active");
+        if (button.classList.contains("active")) {
+            selectAudio.currentTime = 0;
+            selectAudio.play();
+        } else {
+            unselectAudio.currentTime = 0;
+            unselectAudio.play();
+        }
         buttons.forEach(button => {
             if (button.classList.contains("active")) {
                 count++;  
             };
         });
         generateButton.textContent = "Generate (" + count + ")";
+        if (count == 12) {
+            generateButton.style.backgroundColor = "green";
+        } else {
+            generateButton.style.backgroundColor = "red";
+        }
         count = 0;
     });
 }); 
